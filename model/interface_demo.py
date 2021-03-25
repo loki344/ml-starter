b1fdac5c2ac2b442b7c0a3ae27896179653c70ee
loki344 <robert.schlittler@gmx.ch>
2021-03-25 17:06:52 +0100

    - adds demo of usage

--- model/interface_demo.py
+++ model/interface_demo.py
@@ -3,5 +3,5 @@ from model_creation import create_model_class
 
 ## now dynamic
 dynamic_model = create_model_class("/home/robert/Repositories/ml-starter/model/test_input_method.py")
-output = dynamic_model.run([[5, 2.9, 1, 0.2], [5, 2.9, 1, 0.2]], "/home/robert/Repositories/ml-starter/pythontest/iris-flower/iris.onnx")
+output = dynamic_model.run([[5, 2.9, 1, 0.2], [5, 2.9, 1, 0.2]], "/home/robert/Repositories/ml-starter/sample-models/iris.onnx")
 print(output)
