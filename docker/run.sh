b056ce43338c150989a18bda83a3692c25c953b1
loki344 <robert.schlittler@gmx.ch>
2021-03-28 14:06:46 +0200

    - enabled docker-compose - redesigned core functions

---
+++ docker/run.sh
@@ -0,0 +1,3 @@
+cp -u ../requirements.txt ./ml-starter-backend
+cp -u -r ../app ./ml-starter-backend
+docker-compose up
--- run.sh
+++
@@ -1,2 +0,0 @@
-docker build -t ml-starter .
-docker run -d --name ml-starter -p 80:80 -v /home/robert/Repositories/ml-starter/external-folder:/mounted_volume ml-starter:latest
