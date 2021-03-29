39ed7478c479f7fb551143231e754c548734ab42
loki344 <robert.schlittler@gmx.ch>
2021-03-30 00:33:18 +0200

    - enable onnx output names parameter

--- app/model_creation.py
+++ app/model_creation.py
@@ -1,6 +1,6 @@
 from custom_model import CustomModel
 
+#TODO define type of outputnames
+def create_model(onnx_file_path: str, onnx_output_names=[]):
 
-def create_model(onnx_file_path: str):
-
-    return CustomModel(onnx_file_path)
+    return CustomModel(onnx_file_path, onnx_output_names)
