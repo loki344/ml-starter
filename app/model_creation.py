d8bfe6c7d1adc0581c23140193e8844d90c3e5c8
loki344 <robert.schlittler@gmx.ch>
2021-04-03 13:04:34 +0200

    - install dependencies via custom requirements file

--- app/model_creation.py
+++ app/model_creation.py
@@ -1,34 +1,22 @@
 import subprocess
 import sys
+from custom_model.custom_model import CustomModel
+from file_helper import get_file
 
-from custom_model import CustomModel
+
 
 
 #TODO define type of outputnames
 def create_model(onnx_file_path: str, model_output_names=[]):
-   # install_dependencies("../external-folder/gpt2-example/custom_model.py")
 
+    #TODO is only needed if it runs locally?
+    #install_dependencies()
     return CustomModel(onnx_file_path, model_output_names)
 
 
+def install_dependencies():
 
-def install_dependencies(source_code_file_path: str):
-    #TODO Handle with ast instead of string parsing
-    source_code_text = read_file(source_code_file_path)
-    source_code_lines = source_code_text.split("\n")
-    packages = []
+    path_to_custom_requirements = get_file('custom_requirements.txt')
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", path_to_custom_requirements])
 
-    for line in source_code_lines:
-        first_word = line.split(" ")[0]
-        if first_word in ["import", "from"]:
-            packages.append(line.split(" ")[1])
 
-    for package in packages:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-    pass
-
-def read_file(file_path: str):
-    file = open(file_path, "r")
-    text = file.read()
-    return text
