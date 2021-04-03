0e93e6d3154dabd6b3bee1f90a2dd1efc226d2ec
loki344 <robert.schlittler@gmx.ch>
2021-04-03 13:40:00 +0200

    - opencv headless added, solves issue in docker with opencv - made a simple switch to install dependencies in docker via dockerfile and locally via python script

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,9 @@ WORKDIR /app
 COPY app .
 COPY requirements.txt ./requirements.txt
 
+ENV IS_IN_DOCKER=true
 ENV PORT=$PORT
 EXPOSE $PORT
-#opencv fix https://stackoverflow.com/questions/55313610/importerror-libgl-so-1-cannot-open-shared-object-file-no-such-file-or-directo
-RUN apt-get update
-RUN apt-get install ffmpeg libsm6 libxext6  -y
-#end
 
 RUN pip3 install -r requirements.txt
 RUN pip3 install -r ./custom_model/custom_requirements.txt
--- app/model_creation.py
+++ app/model_creation.py
@@ -1,22 +1,21 @@
+import os
 import subprocess
 import sys
 from custom_model.custom_model import CustomModel
 from file_helper import get_file
 
 
-
-
 #TODO define type of outputnames
 def create_model(onnx_file_path: str, model_output_names=[]):
 
-    #TODO is only needed if it runs locally?
-    #install_dependencies()
+    install_dependencies()
     return CustomModel(onnx_file_path, model_output_names)
 
 
 def install_dependencies():
 
-    path_to_custom_requirements = get_file('custom_requirements.txt')
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", path_to_custom_requirements])
+    if not os.environ.get('IS_IN_DOCKER', False):
+        path_to_custom_requirements = get_file('custom_requirements.txt')
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", path_to_custom_requirements])
 
 
