e926e0a876b965c7b1245a0643ce98cb198868bf
loki344 <robert.schlittler@gmx.ch>
2021-04-03 13:03:27 +0200

    - clean up

--- docker/ml-starter-backend/app/interface_demo.py
+++
@@ -1,9 +0,0 @@
-from model_creation import create_model
-
-model = create_model('../external-folder/efficientnet-example/custom_model.py.onnx', ["Softmax:0"])
-
-with open('/external-folder/efficientnet-example/kitten.txt', 'r') as file:
-    image = file.read()
-
-model.predict(image)
-
--- docker/ml-starter-backend/app/model_creation.py
+++
@@ -1,6 +0,0 @@
-from custom_model import CustomModel
-
-#TODO define type of outputnames
-def create_model(onnx_file_path: str, model_output_names=[]):
-
-    return CustomModel(onnx_file_path, model_output_names)
