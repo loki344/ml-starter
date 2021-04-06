e2cba3020c97fd788db25be0462d8d77e1136df1
loki344 <robert.schlittler@gmx.ch>
2021-04-06 23:45:25 +0200

    - basic routing

--- src/App.js
+++ src/App.js
@@ -1,78 +1,42 @@
 import React, {useEffect, useState} from 'react';
 import LogRocket from 'logrocket';
-import {useDispatch, useSelector} from 'react-redux'
-import {getConfiguration} from "./actions/configurationActions";
-import {InputField} from "./components/InputField";
-import {Form, FormLabel, Button} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from "axios";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import Header from "./components/layout/Header";
+import {InputDataView} from "./components/InputDataView";
+import {PredictionView} from "./components/PredictionView";
+import './App.css'
+import {useSelector} from "react-redux";
 
 const App = () => {
     LogRocket.init('bc26dt/ml-starter-frontend');
 
-
-    const dispatch = useDispatch();
-
     const configuration = useSelector(state => state.configuration)
-    const {applicationName, inputFields, requestObject} = configuration
-
-    const {inputData} = useSelector(state => state.prediction)
-
-
-    useEffect(() => {
-        dispatch(getConfiguration())
-    }, [dispatch])
-
-
-    const [prediction, setPrediction] = useState([])
-
-
-    const clickHandler = async (e) => {
-        e.preventDefault()
-
-        let requestData = String(requestObject.inputData)
-        for (let inputField of inputFields){
-            requestData = requestData.replace(inputField.id, inputData[inputField.id])
-        }
-
-        let requestBody = '{"inputData":'+requestData+'}'
-        requestBody = requestBody.replace('/','\/')
-
-        requestBody = JSON.parse(requestBody)
-        const {data} = await axios.post('http://localhost:8000/predictions', requestBody)
-        console.log(typeof data.prediction)
-        setPrediction( typeof data.prediction === 'object' ? data.prediction : [data.prediction] )
-    }
+    const {applicationName} = configuration
 
 
     return (
-        <div className="App">
-            <h1>{applicationName}</h1>
-            <Form>
-            {inputFields.map((inputField) => (
-                <InputField key={inputField.label} inputField={inputField}>
-                </InputField>
-            ))}
-            <br/>
-            <br/>
-            <Button onClick={clickHandler} type="submit">Start prediction</Button>
-            </Form>
-            <div>
-                <h3>Your prediction:</h3>
-                {
-                    prediction.map((pred) => (
-                        typeof pred === 'object' && pred !== null ?
-                    Object.keys(pred).map(key =>
-                            <div>
-                                <p>{key}: {pred[key]}</p>
-                            </div>)
-                            :
-                                    <p>{pred}</p>
-                            )
-                    )
-                }
-            </div>
-            </div>);
+        <Router>
+        <div className="App" >
+            <Header/>
+        </div>
+        <div className="AppBody">
+        <h1 className="ApplicationName">{applicationName}</h1>
+        <Switch>
+            <Route path="/prediction">
+                <PredictionView />
+            </Route>
+            <Route path="/">
+                <InputDataView />
+            </Route>
+        </Switch>
+        </div>
+        </Router>);
 }
 
 export default App;
