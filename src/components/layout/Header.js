1c2f0903829d9ce73d4d3b87e99f658044675bdf
loki344 <robert.schlittler@gmx.ch>
2021-04-06 23:46:40 +0200

    - header, maybe needed or not

---
+++ src/components/layout/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import {Link} from "react-router-dom"
+
+ class Header extends Component {
+    render() {
+        return (
+                    <nav className="navbar navbar-expand-sm navbar-dark bg-primary">
+                        This could be a header... we'll see if it's needed
+
+                    </nav>
+
+        )
+    }
+}
+
+export default Header;
