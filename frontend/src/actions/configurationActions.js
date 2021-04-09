7cb7659b47c2398fefc78a9084e523fdcd170a46
loki344 <robert.schlittler@gmx.ch>
2021-04-09 17:25:49 +0200

    - updates uri

--- frontend/src/actions/configurationActions.js
+++ frontend/src/actions/configurationActions.js
@@ -2,7 +2,8 @@ import {GET_CONFIGURATION} from "../constants/configurationConstants";
 import axios from 'axios'
 
 export const getConfiguration = () => async (dispatch) =>{
-    const {data} = await axios.get('http://localhost:8000/configs')
+
+    const {data} = await axios.get('/api/configs')
     dispatch({type: GET_CONFIGURATION, payload: data })
 
 }
