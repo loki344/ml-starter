14611d854423e1a097644b3cacc3c860a4f8f366
loki344 <robert.schlittler@gmx.ch>
2021-04-10 14:51:31 +0200

    - cleanup

--- frontend/src/components/AboutView.js
+++ frontend/src/components/AboutView.js
@@ -4,7 +4,7 @@
 export const  AboutView = () => {
 
     return(
-        <div className="AboutView">
+        <div>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ad aliquam, eaque, eligendi, explicabo in laudantium magni maiores omnis placeat porro suscipit voluptatum! Atque blanditiis corporis eaque magnam repellendus, ut.
         </div>
     )
