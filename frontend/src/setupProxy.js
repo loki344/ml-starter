2de736a21a7af54b607e13fd2f92102a7583944c
loki344 <robert.schlittler@gmx.ch>
2021-04-10 14:52:42 +0200

    - changed proxy to service name

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.use(
         '/api/*',
         createProxyMiddleware({
-            target: 'http://localhost:8800',
+            target: 'http://ml-starter-backend:8800',
             changeOrigin: true,
         })
     );
