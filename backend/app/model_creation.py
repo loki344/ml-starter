3071368dea7b2be0951d0010e5fbfe69465fffe8
loki344 <robert.schlittler@gmx.ch>
2021-05-02 13:09:12 +0200

    - adds some logging

--- backend/app/model_creation.py
+++ backend/app/model_creation.py
@@ -9,6 +9,8 @@ from file_helper import get_file
 
 #TODO define type of outputnames
 def create_model(onnx_file_path: str, model_output_names=[]) -> AbstractModel:
+    print("-------------------------------------------------------------------------------------------------------")
+    print("Loading ONNX-Model...")
 
     install_dependencies()
     return CustomModel(onnx_file_path, model_output_names)
@@ -17,7 +19,6 @@ def create_model(onnx_file_path: str, model_output_names=[]) -> AbstractModel:
 def install_dependencies():
 
     if not os.environ.get('IS_IN_DOCKER', False):
-        print("-------------------------------------------------------------------------------------------------------")
         print("Not running in docker, installing dependencies from custom_requirements.txt")
 
         path_to_custom_requirements = get_file('custom_requirements.txt')
