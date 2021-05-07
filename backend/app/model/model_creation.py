fcf9d20ed2265d608460f373e8876920635979eb
loki344 <robert.schlittler@gmx.ch>
2021-05-07 23:17:30 +0200

    - removed model output names - made install dependencies more robust - changed names

--- backend/app/model/model_creation.py
+++ backend/app/model/model_creation.py
@@ -7,25 +7,22 @@ from file_helper import get_file
 from model.abstract_model import AbstractModel
 
 
-def create_model(onnx_file_path: str, model_output_names: str = "") -> AbstractModel:
+def create_model(model_file_path: str) -> AbstractModel:
     """
     Creates a CustomModel object for with the given path and model_output_names. Takes care of the custom dependencies
     in case the application is not running in docker.
 
-    :param onnx_file_path: path where to .onnx file is available
-    :type onnx_file_path: str
-
-    :param model_output_names: this param is used for the onnx InferenceSession, example value: [Softmax:0]
-    :type model_output_names: str
+    :param model_file_path: path where to .onnx file is available
+    :type model_file_path: str
 
     :return AbstractModel class which is used in the FastAPI
     """
 
     print("-------------------------------------------------------------------------------------------------------")
-    print("Loading ONNX-Model...")
+    print("Loading model...")
 
     install_dependencies()
-    return CustomModel(onnx_file_path, model_output_names)
+    return CustomModel(model_file_path)
 
 
 def install_dependencies() -> None:
@@ -37,6 +34,8 @@ def install_dependencies() -> None:
     """
     if not os.environ.get('IS_IN_DOCKER', False):
         print("Not running in docker, installing dependencies from custom_requirements.txt")
+        if not get_file('custom_requirements.txt').is_file():
+            return
 
         path_to_custom_requirements = get_file('custom_requirements.txt')
         subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", path_to_custom_requirements])
